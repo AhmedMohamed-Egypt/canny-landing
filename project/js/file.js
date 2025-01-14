f9baad703b70fd9d46e0ded25d69ebbd966b03fa
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2025-01-14 17:17:18 +0200

    update

--- project/js/file.js
+++ project/js/file.js
@@ -1,14 +1,28 @@
-var mySwiper = new Swiper(".swiper-container", {
+var mySwiper = new Swiper(".expertise .swiper-container", {
   spaceBetween: 0,
   slidesPerView: 3,
   centeredSlides: true,
   loop: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".expertise .swiper-button-next",
+    prevEl: ".expertise .swiper-button-prev",
   },
 });
 
+var mySwiperBlog = new Swiper(".latestBlog .swiper-container", {
+  spaceBetween: 0,
+  slidesPerView: 3,
+  
+  loop: true,
+  navigation: {
+    nextEl: ".latestBlog .swiper-button-next",
+    prevEl: ".latestBlog .swiper-button-prev",
+  },
+});
+
+
+
+//latestBlog
 const scrollNav = ()=>{
   if(window.pageYOffset > 200){
     document.body.classList.add('fixed')
